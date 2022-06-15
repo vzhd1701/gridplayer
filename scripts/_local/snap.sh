55c0c8860fc2d4865da421b843dfcb1361c3ac63
vzhd1701 <vzhd1701@gmail.com>
2022-06-15 19:50:30 +0500

    build(snap): generate VLC plugin cache

--- scripts/_local/snap.sh
+++ scripts/_local/snap.sh
@@ -6,6 +6,14 @@ SCRIPT_DIR="$( cd "$( dirname $0 )" && pwd )"
 
 . "scripts/init_app_vars.sh"
 
+if [ "$1" == "run" ]; then
+    snap run ${APP_ID} "${@:2}"
+    exit 0
+elif [ "$1" == "del" ]; then
+    sudo snap remove ${APP_ID}
+    exit 0
+fi
+
 poetry build -f wheel
 
 bash "$SCRIPTS_DIR/linux_meta/build.sh"
