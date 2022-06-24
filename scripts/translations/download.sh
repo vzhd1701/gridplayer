7bfdaa6af5f5f66711be9f08c36ffc8a7307f112
vzhd1701 <vzhd1701@gmail.com>
2022-06-24 19:39:09 +0500

    build(translations): update crowdin script

--- scripts/translations/download.sh
+++ scripts/translations/download.sh
@@ -6,4 +6,11 @@ SCRIPT_DIR="$( cd "$( dirname $0 )" && pwd )"
 
 . "scripts/init_app_vars.sh"
 
-crowdin.bat download -c "$SCRIPT_DIR/crowdin.yml" --identity ".local/crowdin.yml" --skip-untranslated-files --export-only-approved "$@"
+if [ -n "$1" ]; then
+    echo "Downloading translation for language $1"
+    crowdin.bat download -c "$SCRIPT_DIR/crowdin.yml" --identity ".local/crowdin.yml" --skip-untranslated-strings --export-only-approved --language="$1"
+    exit 0
+fi
+
+echo "Downloading all completed translations"
+crowdin.bat download -c "$SCRIPT_DIR/crowdin.yml" --identity ".local/crowdin.yml" --skip-untranslated-files --export-only-approved
