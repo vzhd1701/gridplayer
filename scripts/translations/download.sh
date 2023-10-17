fed8f60b05c3aa80b1cad1443eafc53e44920fd2
vzhd1701 <vzhd1701@gmail.com>
2023-10-17 18:26:39 +0500

    build(translation): update scripts

--- scripts/translations/download.sh
+++ scripts/translations/download.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$( cd "$( dirname $0 )" && pwd )"
 
 list_proofread() {
     # output: LANG PERCENT
-    crowdin status proofreading --no-progress --no-colors -c "$SCRIPT_DIR/crowdin.yml" --identity ".local/crowdin.yml" | grep "%" | grep -v " 0%" | sed -E "s/[[:space:]]*- (.+): [[:digit:]]+%/\1/"
+    crowdin.bat status proofreading --no-progress --no-colors -c "$SCRIPT_DIR/crowdin.yml" --identity ".local/crowdin.yml" | grep "%" | grep -v " 0%" | sed -E "s/[[:space:]]*- (.+): [[:digit:]]+%/\1/"
 }
 
 if [ "$1" == "list" ]; then
@@ -25,7 +25,7 @@ fi
 
 for CUR_LANG in ${LANUAGES[@]}; do
     echo "Downloading language '$CUR_LANG'"
-    crowdin download -c "$SCRIPT_DIR/crowdin.yml" --identity ".local/crowdin.yml" --skip-untranslated-strings --export-only-approved --language="$CUR_LANG"
+    crowdin.bat download -c "$SCRIPT_DIR/crowdin.yml" --identity ".local/crowdin.yml" --skip-untranslated-strings --export-only-approved --language="$CUR_LANG"
 done
 
 "$SCRIPT_DIR/strip_locations.sh"
