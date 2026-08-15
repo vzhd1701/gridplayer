f5f77c27d69167f2b204603414d2d4cba1c9dfc9
vzhd1701 <vzhd1701@gmail.com>
2026-08-15 19:57:34 +0500

    fix: force xcb on linux becuase libVLC doesnt work with wayland

--- gridplayer/main/init_app.py
+++ gridplayer/main/init_app.py
@@ -7,12 +7,17 @@ from PyQt5.QtWidgets import QApplication, QStyleFactory
 from gridplayer.main.init_icons import init_icon
 from gridplayer.main.init_resources import init_resources
 from gridplayer.main.init_translator import init_translator
+from gridplayer.params import env
 from gridplayer.params.static import FONT_SIZE_MAIN
 from gridplayer.params.theme import apply_theme, on_system_theme_changed
 from gridplayer.utils.darkmode import watch_system_theme
 
 
 def init_app():
+    # Wayland doesnt work with libVLC, forcing xcb
+    if env.IS_LINUX:
+        sys.argv += ["-platform", "xcb"]
+
     app = QApplication(sys.argv)
 
     init_resources()
