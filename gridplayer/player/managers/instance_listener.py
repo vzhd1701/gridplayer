6d5d6de547f62e5383a27611196df2d8b477edc6
vzhd1701 <vzhd1701@gmail.com>
2026-09-01 12:42:00 +0500

    fix: don't start instance listener if single instance is disabled

--- gridplayer/player/managers/instance_listener.py
+++ gridplayer/player/managers/instance_listener.py
@@ -1,6 +1,7 @@
-from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import QTimer, pyqtSignal
 
 from gridplayer.player.managers.base import ManagerBase
+from gridplayer.settings import Settings
 from gridplayer.utils.qt import qt_connect
 from gridplayer.utils.single_instance import Listener
 
@@ -12,11 +13,16 @@ class InstanceListenerManager(ManagerBase):
         super().__init__(**kwargs)
 
         self._instance_listener = Listener()
-        self._instance_listener.start()
 
         qt_connect(
             (self._instance_listener.open_files, self.files_opened),
         )
+
+    def init(self):
+        if not Settings().get("player/one_instance"):
+            return
+
+        QTimer.singleShot(0, self._instance_listener.start)
 
     def cleanup(self):
         self._instance_listener.cleanup()
